bf80cc64f77fae8f552d8804832fbd293f8fcaae
naelrahman <78042150+naelrahman@users.noreply.github.com>
2021-02-08 01:40:17 -0500

    Updated main file
    Updated python file to reflect newly updated protocol. This version now uses a tuple of two matrices (called H1 and H2) as the second element in a tuple that is used in calculating a semi-direct product.

--- Matrix Actions Key Exchange.py
+++ Matrix Actions Key Exchange.py
@@ -56,6 +56,12 @@ class MatrixModP():
                 product_values.append(toInsert)
         
         return MatrixModP(self.prime, *product_values)
+    
+    def __eq__(self, other):
+        if (self.values == other.values):
+            return True
+        else:
+            return False
         
     def output(self):
         start = 0
@@ -77,6 +83,7 @@ class MatrixModP():
         adjusted = (calculation % self.prime)
         return adjusted
 
+
 # xk and xk_tuple square-and-multiply algorithm for efficiency.
 def xk(self, k):
     k_binary = bin(k)
@@ -90,6 +97,8 @@ def xk(self, k):
     
     return value  
 
+# Precondition: "self" should be a 2-tuple with a matrix and tuple.
+#   At the beginning of the procedure, this is ((M, (H1, H2), k)
 def xk_tuple(self, k):
     k_binary = bin(k)
     value = self
@@ -106,13 +115,17 @@ def xk_tuple(self, k):
 
 
 # Semidirect product, as described in Section 2.
+#   How firstTuple and secondTuple are broken down:
+#       The second element of each of these tuples is another 2-tuple,
+#       that contains (H1, H2) to some power..
 def semidirect_product(firstTuple, secondTuple):
-    resultingFirstMatrix = ((secondTuple[1] * firstTuple[0] * secondTuple[1])
+    resultingFirstMatrix = ((secondTuple[1][0] * firstTuple[0] * secondTuple[1][1])
         + secondTuple[0])
     
-    resultingSecondMatrix = (firstTuple[1] * secondTuple[1])
+    resultingH1 = (firstTuple[1][0] * secondTuple[1][0])
+    resultingH2 = (firstTuple[1][1] * secondTuple[1][1])
     
-    return [resultingFirstMatrix, resultingSecondMatrix]
+    return [resultingFirstMatrix, (resultingH1, resultingH2)]
 
 
 
@@ -121,20 +134,23 @@ def semidirect_product(firstTuple, secondTuple):
 # ----------------------------------------------------------------------
 
 # STEP 1:
-# (i) Generating M, H:
+# (i) Generating M, H1, H2:
 allCriteriaFit = False
 
 while (allCriteriaFit == False):
     allCriteriaFit = False
     MATRIX_M_VALUES = []
-    MATRIX_H_VALUES = []
+    MATRIX_H1_VALUES = []
+    MATRIX_H2_VALUES = []
     
     for x in range(9):
         MATRIX_M_VALUES.append(randbelow(PRIME_NUMBER))
-        MATRIX_H_VALUES.append(randbelow(PRIME_NUMBER))
+        MATRIX_H1_VALUES.append(randbelow(PRIME_NUMBER))
+        MATRIX_H2_VALUES.append(randbelow(PRIME_NUMBER))
 
     M = MatrixModP(PRIME_NUMBER, *MATRIX_M_VALUES)
-    H = MatrixModP(PRIME_NUMBER, *MATRIX_H_VALUES)
+    H1 = MatrixModP(PRIME_NUMBER, *MATRIX_H1_VALUES)
+    H2 = MatrixModP(PRIME_NUMBER, *MATRIX_H2_VALUES)
 
     # If M is not invertible, redo generation:
     while (M.determinant() == 0):
@@ -142,29 +158,48 @@ while (allCriteriaFit == False):
         for x in range(9):
             MATRIX_M_VALUES.append(randbelow(PRIME_NUMBER))
 
-        M = MatrixModP(PRIME_NUMBER, *MATRIX_H_VALUES)
+        M = MatrixModP(PRIME_NUMBER, *MATRIX_M_VALUES)
+        print("Failed at M determinant")
 
     # If H is not invertible, redo generation:
-    while (H.determinant() == 0):
-        MATRIX_H_VALUES = []
+    while (H1.determinant() == 0):
+        MATRIX_H1_VALUES = []
         for x in range(9):
-            MATRIX_H_VALUES.append(randbelow(PRIME_NUMBER))
+            MATRIX_H1_VALUES.append(randbelow(PRIME_NUMBER))
+            
+        H1 = MatrixModP(PRIME_NUMBER, *MATRIX_H1_VALUES)
+        print("Failed at H1 determinant")
 
-        H = MatrixModP(PRIME_NUMBER, *MATRIX_H_VALUES)
+    while (H2.determinant() == 0):
+        MATRIX_H2_VALUES = []
+        for x in range(9):
+            MATRIX_H2_VALUES.append(randbelow(PRIME_NUMBER))
+            
+        H2 = MatrixModP(PRIME_NUMBER, *MATRIX_H2_VALUES)
+        print("Failed at H2 determinant")
     
-    # Redoes generation of M and H if they are not invertible:
-    MH = M * H
-    HM = H * M
+    # Redoes generation of M and H1 or H2 if they are not invertible:
+    MH = M * H1
+    HM = H1 * M
     if MH.values == HM.values:
+        print("Failed at H, M1 invertible")        
+        continue 
+    
+    MH = M * H2
+    HM = H2 * M
+    if MH.values == HM.values:
+        print("Failed at H, M2 invertible")                
         continue 
     
     # Ensures order of H^(p+1)(p^2+p+1) != 0
-    orderCheck = xk(H, ((PRIME_NUMBER**2 + PRIME_NUMBER + 1) * (PRIME_NUMBER + 1)))
+    orderCheckH1 = xk(H1, ((PRIME_NUMBER**2 + PRIME_NUMBER + 1) * (PRIME_NUMBER + 1)))
+    orderCheckH2 = xk(H2, ((PRIME_NUMBER**2 + PRIME_NUMBER + 1) * (PRIME_NUMBER + 1)))
     
-    if (orderCheck.values != IDENTITY_MATRIX):
+    if (orderCheckH1.values != IDENTITY_MATRIX and orderCheckH2.values != IDENTITY_MATRIX):
+        print("Checks if identity matrix occurs") 
         allCriteriaFit = True
 
-print("Finished generating M and H")
+print("Finished generating M, H1, H2")
 
 # (ii) Alice and Bob's private selections.
 #   Range of m and n, based on the magnitude of q.
@@ -187,31 +222,31 @@ while (gcd(m, PRIME_NUMBER - 1) != 1 or gcd(n, PRIME_NUMBER - 1) != 1):
 print("Finished generating m and n")
 
 # STEP 2: Alice calculates and sends A.
-AliceCalculation = xk_tuple((M, H), m)
+AliceCalculation = xk_tuple((M, (H1, H2)), m)
 A = AliceCalculation[0]
 
 print("Finished calculating A")
      
 # STEP 3: Bob calculates and sends B.
-BobCalculation = xk_tuple((M, H), n)
+BobCalculation = xk_tuple((M, (H1, H2)), n)
 B = BobCalculation[0]
 
 print("Finished calculating B")
 
-# STEP 4: Alice retrieves K_A by computing (B, x)* (A; H^m).
+# STEP 4: Alice retrieves K_A by computing (B, x)* (A, (H1^m, H2^m)).
 AliceKey = semidirect_product(BobCalculation, AliceCalculation)
 K_A = AliceKey[0]
 
 print("Finished calculating K_A")
 
-# STEP 5: Bob retrieves K_B by computing (A, y)* (B; H^n).
+# STEP 5: Bob retrieves K_B by computing (A, y)* (B, (H1^n, H2^n)).
 BobKey = semidirect_product(AliceCalculation, BobCalculation)
 K_B = BobKey[0]
 
 print("Finished calculating K_B")
 
 # STEP 6: Calculation of secret key K to confirm key generation works.
-KeyCalculation = xk_tuple((M, H), m + n)
+KeyCalculation = xk_tuple((M, (H1, H2)), m + n)
 K = KeyCalculation[0]
 
 print("Finished calculating K")
@@ -225,8 +260,11 @@ print("\n------------")
 print("\nGenerated M:")
 M.output()
 
-print("\nGenerated H:")
-H.output()
+print("\nGenerated H1:")
+H1.output()
+
+print("\nGenerated H2:")
+H2.output()
 
 print("\n------------")
 
@@ -238,3 +276,10 @@ K_B.output()
 
 print("\nShared Secret Key (K)")
 K.output()
+
+if (K_A == K):
+    print("\nAlice's Key is equal to the Actual Key")
+    
+if (K_B == K):
+    print("Bob's Key is equal to the Actual Key")
+
